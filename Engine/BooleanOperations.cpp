37341e527cede77fefb7ebda8c0ac34f024cafb7
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-07-21 12:12:29 +0700

    added Boolean class

---
+++ Engine/BooleanOperations.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "BooleanOperations.h"
+
+void BooleanOperations::OnCut()
+{
+
+}
+
+void BooleanOperations::OnFuse()
+{
+}
---
+++ Engine/BooleanOperations.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class BooleanOperations
+{
+	void OnCut();
+	void OnFuse();
+};
+
