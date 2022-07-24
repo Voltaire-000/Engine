a1e72dadb44b8331bb3b1a934948309e1b655c45
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-07-24 17:45:07 +0700

    edges display viewcube works material works, edge selection not working

--- Engine/Engine_Doc_Viewer.cpp
+++ Engine/Engine_Doc_Viewer.cpp
@@ -261,6 +261,7 @@ void CEngineDoc::MakeCut()
 
 	//m_context->Erase(, true);
 	m_context->EraseAll(true);
+	m_context->Activate(2, true);
 	m_context->Display(shape, true);
 	m_context->Display(m_viewcube, true);
 
