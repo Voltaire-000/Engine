3cdedfcd5178b3da576ee3221e0d77271e690cb4
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-07-25 18:46:05 +0700

    update class

--- Engine/SampleProfile.cpp
+++ Engine/SampleProfile.cpp
@@ -1,4 +1,28 @@
 #include "pch.h"
 #include "SampleProfile.h"
 
+CSampleProfile::CSampleProfile() noexcept
+{
+}
 
+CSampleProfile::CSampleProfile()
+{
+}
+
+TopoDS_Shape CSampleProfile::MkProfile(const Standard_Real Rc,
+	const Standard_Real L_cyl,
+	const Standard_Real b,
+	const Standard_Real R2,
+	const Standard_Real R1,
+	const Standard_Real Rt,
+	const Standard_Real Lc,
+	const Standard_Real Rn,
+	const Standard_Real Tn,
+	const Standard_Real Te,
+	const Standard_Real Re,
+	const Standard_Real Le,
+	const Standard_Real Wt,
+	const Standard_Real L_Star)
+{
+	return TopoDS_Shape();
+}
