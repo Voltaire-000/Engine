433b2385c697ace1fae7f767d0e6b3ff058c7262
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-07-25 19:45:54 +0700

    restart

--- Engine/SampleProfile.cpp
+++ Engine/SampleProfile.cpp
@@ -24,5 +24,21 @@ TopoDS_Shape CSampleProfile::MkProfile(const Standard_Real Rc,
 	const Standard_Real Wt,
 	const Standard_Real L_Star)
 {
+	//	Origin
+	Standard_Real xOrigin = 0;
+	Standard_Real yOrigin = 0;
+	Standard_Real zOrigin = 0;
+	//	Z is up
+	gp_Pnt aPnt1(xOrigin, yOrigin, Lc + Le);
+	gp_Pnt aPnt2(Rc, yOrigin, Lc + Le);
+	gp_Pnt aPnt3(Rc, yOrigin, (Lc + Le) - L_cyl);	/*start of R2 radius*/
+	// begin contraction radius at aPnt2
+	// end radius at aPnt4
+	gp_Pnt aPnt4(Rc - (101.7), yOrigin, (Lc + Le) - L_cyl - (Rc - (101.7)));	/*end of R2 radius*/
+
+	//gp_Pnt aPntR1Start()
+	//gp_Pnt aPntR1Center()
+	//gp_Pnt aPntR1End()
+
 	return TopoDS_Shape();
 }
