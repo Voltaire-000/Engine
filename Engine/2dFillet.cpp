92daeee5f5ed7a5bbc1eea8cac71b2466b64c383
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-07-28 12:17:46 +0700

    in process

--- Engine/2dFillet.cpp
+++ Engine/2dFillet.cpp
@@ -1,2 +1,36 @@
 #include "pch.h"
 #include "2dFillet.h"
+
+C2dFillet::C2dFillet() noexcept
+{
+}
+
+C2dFillet::~C2dFillet()
+{
+}
+
+TopoDS_Shape C2dFillet::MkFilletProfile()
+{
+	Standard_Real xOrigin = 0;
+	Standard_Real yOrigin = 0;
+
+	//	4 points of square
+	gp_Pnt2d aPnt1(xOrigin, yOrigin);
+	gp_Pnt2d aPnt2(xOrigin + 100, yOrigin);
+	gp_Pnt2d aPnt3(aPnt2.X(), yOrigin + 100);
+	gp_Pnt2d aPnt4(xOrigin, aPnt3.Y());
+
+	gp_Ax2d axis = gp::OY2d();
+
+	//	make the segments
+	Handle(Geom2d_TrimmedCurve) aSegment1 = GCE2d_MakeSegment(aPnt1, aPnt2);
+	Handle(Geom2d_TrimmedCurve) aSegment1 = GCE2d_MakeSegment(aPnt2, aPnt3);
+	Handle(Geom2d_TrimmedCurve) aSegment1 = GCE2d_MakeSegment(aPnt3, aPnt4);
+	Handle(Geom2d_TrimmedCurve) aSegment1 = GCE2d_MakeSegment(aPnt4, aPnt4);
+
+	//	make the edges
+	TopoDS_Edge anEdge1 = BRepBuilderAPI_MakeEdge(aSegment1);
+
+
+	return TopoDS_Shape();
+}
--- Engine/2dFillet.h
+++ Engine/2dFillet.h
@@ -1,5 +1,10 @@
 #pragma once
 class C2dFillet
 {
+public:
+	C2dFillet() noexcept;
+	virtual ~C2dFillet();
+
+	TopoDS_Shape MkFilletProfile();
 };
 
